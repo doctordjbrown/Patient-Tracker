a19f5bf93c116bb997dcd4699fbc723056ae4f42
DJ Brown <dj@hcv.cc>
2011-03-07 17:29:09 +0000

    master

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PatientTracker::Application.config.secret_token = '6325f0a0d02fcd4d3c72ad4dd9050d22d0258dd1bd7a8bb3215732370e597093a55a550979a7ea785d9528588b9dbf26b39db0936ac22cfeca8ec11b41c699eb'
